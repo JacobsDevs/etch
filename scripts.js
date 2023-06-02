53c057bed51bcf8d8fe08c0d4f44c3a1eaee7ea7
Jacob <jacobphelan@hotmail.com>
2023-06-02 10:34:20 +1000

    Add Reset, Click to draw and Color Palette Sidebar
    - Cleaned up the code and added more verbose comments
    
    - Moved the color picker to the right as a column to let it
    sit on the screen cleaner and increase support for smaller screens
    
    - Halved the size of the grid in pixels to allow for a smaller area
    to work on.
    
    - Added the reset button to clear the grid without resetting color palette

--- scripts.js
+++ scripts.js
@@ -1,57 +1,57 @@
-/**
-const container = document.querySelector('#container');
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content Made by Jacob!!';
+/**ETCH A SKETCH
+ * This page will be a basic etch a sketch with 10 randomized colors options.  Future options will be added
+*/
 
-container.appendChild(content);
- */
+//ESTABLISHING VARS
+//First color is Black
+let chosenColor = "black";
 
-//Establish our components
-const newDiv = document.createElement('button');
-const body = document.querySelector('body');
-newDiv.textContent = "New Size Grid?";
-body.appendChild(newDiv);
+//Checks if mouse is pressed for the drawing function
+let mousePressedDown = false;
+document.addEventListener("mousedown", () => {mousePressedDown = true;});
+document.addEventListener("mouseup", () => {mousePressedDown = false;});
+
+//Initial size of drawing grid.  Can be changed to a maximum of 100
 let squareChoice  = 16;
-const newSizeButton  = document.querySelector('button');
-newSizeButton.style.marginBottom = '16px';
-newSizeButton.addEventListener("click", newSize)
+
+//Establish our body for appending
+const body = document.querySelector('body');
+
+//BUTTON DIV
+const buttonDiv = document.createElement("div");
+buttonDiv.classList.add("buttondiv")
+body.appendChild(buttonDiv)
+
+//NEW SIZE BUTTON
+//Create the New Size Grid Button
+const newButton = document.createElement('button');
+newButton.textContent = "New Size Grid?";
+newButton.style.marginBottom = '16px';
+buttonDiv.appendChild(newButton);
+//Setup newSizeButton event and function
+newButton.addEventListener("click", newSize)
+
+//RESET BUTTON
+//Create the reset button
+const resetButton = document.createElement('button');
+resetButton.textContent = "RESET";
+resetButton.style.marginBottom = '16px';
+buttonDiv.appendChild(resetButton);
+//Setup reset event
+resetButton.addEventListener("click", resetGrid)
+
+//Create the canvas container to have color options on the 
+//Right of the canvas
+const canvasContainer = document.createElement('div')
+canvasContainer.classList.add('canvasContainer')
+body.appendChild(canvasContainer)
+
 const container = document.createElement('div');
-body.appendChild(container);
-let chosenColor = "red";
+container.classList.add("container")
+canvasContainer.appendChild(container);
 
 //Update is called to regenerate the grid at the desired size.  This runs on load to draw original 16 x 16 grid
 update();
-/*16 x 16 grid of square divs into container*/
-
-
-//This section adds the Random Color Palette down the bottom. This will make a new palette every load.
-function getRandomColor() {
-    return Math.floor(Math.random() * 255);
-}
-
-const colorContainer = document.createElement('div');
-colorContainer.classList.add("colorcontainer")
-for (let i = 0; i < 10; i++){
-    const colorSquare = document.createElement('div');
-    colorSquare.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
-    colorSquare.classList.add("colorsquare")
-    colorContainer.appendChild(colorSquare);
-}
-body.appendChild(colorContainer)
-
-const colors = document.querySelectorAll('.colorsquare');
-colors.forEach((color) => {
-    color.addEventListener("click", newColorChosen);
-})
-
-function newColorChosen() {
-    chosenColor = this.style.backgroundColor;
-}
-
-
-//Used to update the size of the box.  Also used on 
-//first load to get the initial size
 function update() {
     for (let i = 0; i < squareChoice; i++) {
     const newRow = document.createElement('div');
@@ -63,17 +63,49 @@ function update() {
     }
     container.appendChild(newRow);
     }
-
+    //Setup the drawing function for each square of the grid
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
-    square.addEventListener("mouseenter", hovering);
+        square.addEventListener("mousemove", paint);
     })
 }
 
-//Colour's the square that is hovered over
-function hovering() {
-    this.style.backgroundColor = chosenColor;
+
+//Function for random RGB values
+function getRandomColor() {
+    return Math.floor(Math.random() * 255);
 }
+
+//Create the colorContainer div and populate with random colors
+const colorContainer = document.createElement('div');
+colorContainer.classList.add("colorcontainer")
+for (let i = 0; i < 10; i++){
+    const colorSquare = document.createElement('div');
+    colorSquare.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    colorSquare.classList.add("colorsquare")
+    colorContainer.appendChild(colorSquare);
+}
+canvasContainer.appendChild(colorContainer)
+
+//Setup color chooser to change draw color to new color
+const colors = document.querySelectorAll('.colorsquare');
+colors.forEach((color) => {
+    color.addEventListener("click", newColorChosen);
+})
+
+function newColorChosen() {
+    chosenColor = this.style.backgroundColor;
+}
+
+
+
+//Colour's the square that is hovered over
+function paint() {
+        if (mousePressedDown == true) {
+            this.style.backgroundColor = chosenColor;
+}}
+
+
 
 //New size button with number < 100 validation
 function newSize() {
@@ -88,4 +120,10 @@ function newSize() {
     container.innerHTML  = "";
     update();
     
+}
+
+//Reset the grid to all white
+function resetGrid() {
+    container.innerHTML = "";
+    update()
 }
